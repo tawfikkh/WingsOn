79d26e72fcf6b6c4f1c10021f1f749d99f40f801
Tawfik Khalifeh <tawfik.khalifeh@gotocme.com>
2019-02-25 15:22:31 +0200

    new service to list mail passengers

--- WingsOn.Api/Controllers/PeopleController.cs
+++ WingsOn.Api/Controllers/PeopleController.cs
@@ -4,23 +4,27 @@ using WingsOn.Api.DTOs.Request;
 using WingsOn.Api.DTOs.Response;
 using WingsOn.Dal;
 using WingsOn.Domain;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace WingsOn.Api.Controllers
 {
     [RoutePrefix("api/People")]
     public class PeopleController : BaseController
     {
-        private readonly IRepository<Person> _repo;
+        private readonly IRepository<Person> _personRepo;
+        private readonly IRepository<Booking> _bookingRepo;
 
-        public PeopleController(IRepository<Person> repo)
+        public PeopleController(IRepository<Person> personRepo, IRepository<Booking> bookingRepo)
         {
-            _repo = repo;
+            _personRepo = personRepo;
+            _bookingRepo = bookingRepo;
         }
 
         // GET api/<controller>/5
         public IHttpActionResult GetPersonById(int id)
         {
-            var person = _repo.Get(id);
+            var person = _personRepo.Get(id);
             if (person == null)
             {
                 return NotFound();
@@ -29,18 +33,29 @@ namespace WingsOn.Api.Controllers
             return Ok(person);
         }
 
+        [Route("MalePassengers")]
+        public IHttpActionResult GetAllMalePassengers()
+        {
+            var passengers = _bookingRepo
+                .GetAll()
+                .SelectMany(b => b.Passengers.Where(p => p.Gender == GenderType.Male));
+
+            var result = Mapper.Map<IEnumerable<PersonDtoRes>>(passengers);
+            return Ok(result);
+        }
+
         // PATCH api/<controller>/id
         [HttpPatch]
         public IHttpActionResult PatchPersonEmail(int id, [FromBody]PersonDtoReq model)
         {
-            var person = _repo.Get(id);
+            var person = _personRepo.Get(id);
             if (person == null)
             {
                 return NotFound();
             }
 
             person.Email = model.Email;
-            _repo.Save(person);
+            _personRepo.Save(person);
 
             PersonDtoRes response = Mapper.Map<PersonDtoRes>(person);
             return Ok(person);
